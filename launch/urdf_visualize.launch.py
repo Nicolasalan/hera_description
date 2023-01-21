0ee1d3ff8e3715c14736ae1383e64593c130ef47
Nicolas Alan <nicolasalangrotti03@gmail.com>
2023-01-21 11:27:05 -0300

    update launch file

--- launch/urdf_visualize.launch.py
+++ launch/urdf_visualize.launch.py
@@ -1,36 +1,32 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import Command
 from launch_ros.actions import Node
+import xacro
 
-# this is the function launch  system will look for
+
 def generate_launch_description():
 
-    ####### DATA INPUT ##########
-    urdf_file = 'hera.urdf'
+    # Specify the name of the package and path to xacro file within the package
+    pkg_name = 'hera_description'
+    file_subpath = 'robot/hera.urdf.xacro'
 
-    package_description = "hera_description"
 
-    ####### DATA INPUT END ##########
-    print("Fetching URDF ==>")
-    robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", urdf_file)
+    # Use xacro to process the file
+    xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
+    robot_description_raw = xacro.process_file(xacro_file).toxml()
 
-    # Robot State Publisher
 
-    robot_state_publisher_node = Node(
+    # Configure the node
+    node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        name='robot_state_publisher_node',
-        emulate_tty=True,
-        parameters=[{'use_sim_time': True, 'robot_description': Command(['xacro ', robot_desc_path])}],
-        output="screen"
+        output='screen',
+        parameters=[{'robot_description': robot_description_raw}] # add other parameters here if required
     )
 
-    # create and return launch description object
-    return LaunchDescription(
-        [            
-            robot_state_publisher_node
-        ]
-    )
+
+    # Run the node
+    return LaunchDescription([
+        node_robot_state_publisher
+    ])
